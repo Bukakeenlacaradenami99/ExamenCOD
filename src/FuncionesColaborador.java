7249b0a599032d4685fe930f894a36b688daee5a
lapollazadelawXXL <158739419+lapollazadelawXXL@users.noreply.github.com>
2024-03-03 10:55:36 +0100

    Creando clase colaborador B

--- src/FuncionesColaborador.java
+++ src/FuncionesColaborador.java
@@ -1,4 +1,21 @@
-public class FuncionesColaborador {
+import java.util.*;
+public class FuncionesColaborador extends Lider2 {
+
+    private static ArrayList <Lider2> mistrabajadores = new ArrayList <>();
+    public FuncionesColaborador(String nombre, String apellido, String NIF, int edad, int num_afiliado, String departamento) {
+        super(nombre, apellido, NIF, edad, num_afiliado, departamento);
+    }
+
+
+    public void DardealtaTrabajador(ArrayList <Lider2> lista){
+
+
+        lista.add(new Lider2("Juan", "Vazquez Fariña", "34567832G", 50, 9182738, "Comercio Internacional"));
+
+
+
+
+    }
 
 
 
