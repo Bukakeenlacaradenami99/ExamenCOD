a92c81efd5653b549a4241b2b803a6b829680243
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2024-03-03 10:29:14 +0100

    Constructor clase lider2 A

--- src/Lider2.java
+++ src/Lider2.java
@@ -1,4 +1,15 @@
-public class Lider2 {
+public class Lider2 extends Lider {
+
+    private int num_afiliado;
+    private String departamento;
+
+    public Lider2(String nombre, String apellido, String NIF, int edad, int num_afiliado, String departamento) {
+        super(nombre, apellido, NIF, edad);
+        this.num_afiliado = num_afiliado;
+        this.departamento = departamento;
+    }
+
+
 
 
 }
